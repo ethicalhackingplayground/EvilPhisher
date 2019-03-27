64751f015e56d4e4edac289537c2d4e11dc24a18
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-03-27 18:43:13 -0400

    Delete signin.js

--- myspace/index_files/signin.js
+++
@@ -1,1 +0,0 @@
-/* account */(function(){function b(){a=setTimeout(function(){comm.send("dialog.isOpen",function(a){a===0&&comm.send("location.href","/")})},0)}var a=null;comm.listen("dialog.hiding",b),comm.listen("spaceway.page.loading",function(){clearTimeout(a),comm.stopListening("dialog.hiding",b)}),comm.send("login.show",{context:{action:"signInPage"}})})();
