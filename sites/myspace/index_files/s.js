96ebf242822af4b23c7675dd8b78a4610c310709
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2019-03-27 18:25:02 -0400

    Add files via upload

---
+++ sites/myspace/index_files/s.js
@@ -0,0 +1,1 @@
+!function(a){var b="_viantTMC";b in window&&window[b].exec?window[b].exec(a):console.log("Failed execute vtm callback")}({t:"G7HXJhbVPjgFhB",app:"vtm",v:"1.2", ss:"0", sn:"pdx1-ucid101.ads"});
